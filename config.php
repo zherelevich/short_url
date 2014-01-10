ca5f7c72fc0be972c11e428845ee858c58b1d15f
zherelevich <zherelevich@mail.ru>
2014-01-10 09:53:51 +0600

    upd

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//Домен короткой ссылки
+define('DOMIAN', 'http://uj.qw');
+//Начало ссылки
+define('START_LINK', 'uj');
+//Каталоги
+define('DIR_ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('DIR_CONTROLLER', DIR_ROOT.'/controller');
+define('DIR_MODEL', DIR_ROOT.'/model');
+define('DIR_VIEW', DIR_ROOT.'/view');
+//DB
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cutter');
+?>
